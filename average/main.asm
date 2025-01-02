291a6d66a063d4cfc7e138b66ffa964100fe758d
Danny1234561111 <154524524+Danny1234561111@users.noreply.github.com>
2025-01-02 16:16:37 +0800

    Update main.asm

--- average/main.asm
+++ average/main.asm
@@ -2,48 +2,54 @@ section .data
     x dd 5, 3, 2, 6, 1, 7, 4
     y dd 0, 10, 1, 9, 2, 8, 5
     n equ 7  ; количество элементов в массивах
-    sum dd 0
-    mean dd 0
+    sum dq 0
+    mean dq 0
     out_file db 'out', 0
 
 section .text
     global _start
 
 _start:
-    xor ecx, ecx  ; индекс i = 0
-    xor ebx, ebx  ; сумма = 0
+    xor rcx, rcx  ; индекс i = 0
+    xor rbx, rbx  ; сумма = 0
 
 .loop:
-    cmp ecx, n
+    cmp rcx, n
     jge .done
 
-    mov eax, [x + ecx*4]
-    sub eax, [y + ecx*4]
-    add ebx, eax
-    inc ecx
+    mov eax, [x + rcx*4]
+    mov edx, [y + rcx*4]
+    sub eax, edx
+    add rbx, rax
+    inc rcx
     jmp .loop
 
 .done:
-    mov eax, ebx
-    cdq
-    idiv dword [n]
-    mov [mean], eax
+    mov rax, rbx
+    idiv qword [n] ; правильное деление, без cqo
+    mov [mean], rax
 
-    mov eax, 5      ; sys_open
-    mov ebx, out_file
-    mov ecx, 1      ; O_WRONLY
-    mov edx, 0
-    int 0x80
-    mov ebx, eax
+    ; syscall open
+    mov rax, 2     ; sys_open
+    mov rdi, out_file
+    mov rsi, 0x2      ; O_WRONLY
+    mov rdx, 0
+    syscall
+    mov rbx, rax
 
-    mov eax, 4      ; sys_write
-    mov ecx, mean
-    mov edx, 4
-    int 0x80
+    ; syscall write
+    mov rax, 1     ; sys_write
+    mov rdi, rbx
+    mov rsi, mean
+    mov rdx, 8  ; Количество байтов для записи
+    syscall
 
-    mov eax, 6      ; sys_close
-    int 0x80
+    ; syscall close
+    mov rax, 3     ; sys_close
+    mov rdi, rbx
+    syscall
 
-    mov eax, 1      ; exit
-    xor ebx, ebx
-    int 0x80
+    ; exit
+    mov rax, 60    ; sys_exit
+    xor rdi, rdi
+    syscall
